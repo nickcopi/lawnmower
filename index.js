7b44aab5169d9e1aba5d43208d3536dc785b3edb
Nicholas Copi <copinf@vcu.edu>
2019-07-29 14:02:42 -0400

    Working stuff, wow.

--- index.js
+++ index.js
@@ -7,14 +7,19 @@ const Directions = {
 	DOWN:4
 }
 
+const Items = {
+	GRASS:0
+}
+
 class Game{
 	constructor(canvas){
 		this.canvas = canvas;
 		this.doBuy.bind(this);
-		this.scene = new GrassScene(canvas,game);
 		this.wallet = {
-			money:0
+			money:0,
+			items: new ItemManager()
 		};
+		this.scene = new GrassScene(canvas,this.wallet);
 		window.addEventListener('keypress',e=>{
 			switch(e.key){
 				case 'w':
@@ -44,21 +49,20 @@ class Scene{
 		this.ctx = canvas.getContext('2d');
 	}
 	collide(o1,o2){
-		return (o1.x < o2.x + o2.width && o1.x + o1.width > o2.x && o1.y < o2.y + o2.height && o1.y + o1.height > o2.y) && o1 !== o2;
+		return (o1.x < o2.x + o2.width && o1.x + o1.getWidth() > o2.x && o1.y < o2.y + o2.height && o1.y + o1.getHeight() > o2.y) && o1 !== o2;
 	}
 
 }
 
 class GrassScene extends Scene{
-	constructor(canvas,game){
+	constructor(canvas,wallet){
 		super(canvas);
-		this.game = game;
 		this.grasses = [];
 		this.ui = {};
 		this.tick = 0;
 		this.populateGrass(150);
 		this.player = new Player(250,250);
-		this.setupMenu();
+		this.setupMenu(wallet);
 		this.interval = setInterval(()=>{
 			this.update();
 			this.updateMenu();
@@ -70,7 +74,7 @@ class GrassScene extends Scene{
 		this.checkCollisions();
 		this.tick++;
 	}
-	setupMenu(){
+	setupMenu(wallet){
 		/*setup UI holder*/
 		this.ui.baseUI = document.getElementById('UIHolder');
 		this.ui.baseUI.innerHTML = '';
@@ -105,9 +109,41 @@ class GrassScene extends Scene{
 			});
 			this.ui.baseUI.appendChild(upgrade);
 		});
-	
+		
+		/*Generate items*/
+		wallet.items.list.forEach((item,i)=>{
+			const baseLeft = 12.5 + 20;
+			const baseTop = 512.5 + 10; 
+			let x = baseLeft + (i*30)%270;
+			let y = baseTop + (Math.floor((i*30)/270) * 30);
+			item.img.style.left = x + 'px';
+			item.img.style.top = y + 'px';
+			item.img.className = 'item';
+			let hoverBox;
+			item.img.addEventListener('mouseover',(e)=>{
+				hoverBox = document.createElement('div');
+				hoverBox.className = 'hoverBox';
+				hoverBox.style.left = e.clientX - 40 + 'px';
+				hoverBox.style.top = e.clientY - 100 + 'px';
+				hoverBox.innerText = `Grass\n Sell price: ${item.cost}`;
+				this.ui.baseUI.appendChild(hoverBox);
+			});
+			item.img.addEventListener('mouseout',(e)=>{
+				hoverBox.remove();
+			});
+			item.img.addEventListener('click',e=>{
+				item.sell(game.wallet);
+			});
+			let counter = document.createElement('div');
+			counter.className = 'itemCounter';
+			counter.style.left = x + 15 + 'px';
+			counter.style.top = y + 15 + 'px';
+			this.ui.baseUI.appendChild(item.img);
+			this.ui.baseUI.appendChild(counter);
+		});
 		/*add references to UI object*/
 		this.ui.upgradeUI = [...document.querySelectorAll('.upgrade')];
+		this.ui.counters = [...document.querySelectorAll('.itemCounter')];
 		this.ui.moneyDisplay = document.getElementById('money');
 		this.ui.rightBar = rightBar;
 		this.ui.bottomBar = bottomBar;
@@ -119,6 +155,9 @@ class GrassScene extends Scene{
 		this.ui.upgradeUI.forEach((ui,i)=>{
 			let upgrade = this.player.upgrades.list[i];
 			ui.innerHTML = `<b>${upgrade.name}</b><br>${upgrade.getText()}<br>$${upgrade.getCost()}`;
+		});
+		this.ui.counters.forEach((counter,i)=>{
+			counter.innerText = game.wallet.items.list[i].count;
 		});
 		this.ui.moneyText.innerText = `Money: $${game.wallet.money}`;
 	}
@@ -133,10 +172,17 @@ class GrassScene extends Scene{
 			ctx.fillRect(g.x,g.y,g.width,g.height);
 		});
 		ctx.fillStyle = 'white';
-		ctx.fillRect(this.player.x,this.player.y,this.player.width,this.player.height);
+		ctx.fillRect(this.player.x,this.player.y,this.player.getWidth(),this.player.getHeight());
 	}
 	checkCollisions(){
-		this.grasses = this.grasses.filter(g=>!this.collide(g,this.player));
+		this.grasses = this.grasses.filter(harvest=>{
+			let didCollide = this.collide(this.player,harvest)
+			if(didCollide){
+				game.wallet.items.list[harvest.type].count++;
+			}
+			return !didCollide;
+		});
+		if(!this.grasses.length) this.populateGrass(150);
 	}
 	populateGrass(num){
 		if(num <= 0) return;
@@ -147,7 +193,8 @@ class GrassScene extends Scene{
 }
 
 
-class Grass{
+
+class Harvestable{
 	constructor(x,y,width,height){
 		this.x = x;
 		this.y = y;
@@ -156,18 +203,31 @@ class Grass{
 	}
 }
 
+class Grass extends Harvestable{
+	constructor(x,y,width,height){
+		super(x,y,width,height);
+		this.type = Items.GRASS;
+	}
+}
+
 class Player{
 	constructor(x,y){
 		this.x = x;
 		this.y = y;
-		this.width = 30;
-		this.height = 30;
+		this.baseWidth = 30;
+		this.baseHeight = 30;
 		this.direction = Directions.RIGHT;
 		this.baseSpeed = 3;
 		this.upgrades = new UpgradeManager();
 	}
 	getSpeed(){
 		return this.baseSpeed + this.baseSpeed * this.upgrades.speedUpgrade.getBonus();
+	}
+	getHeight(){
+		return this.baseHeight + this.baseHeight * this.upgrades.sizeUpgrade.getBonus();
+	}
+	getWidth(){
+		return this.baseWidth + this.baseWidth * this.upgrades.sizeUpgrade.getBonus();
 	}
 	move(){
 		switch(this.direction){
@@ -185,16 +245,16 @@ class Player{
 			break;
 		}
 		if(this.x < 0) this.x = 0;
-		if(this.x > canvas.width - this.width) this.x = canvas.width-this.width;
+		if(this.x > canvas.width - this.getWidth()) this.x = canvas.width-this.getWidth();
 		if(this.y < 0) this.y = 0;
-		if(this.y > canvas.height - this.height) this.y = canvas.height-this.height;
+		if(this.y > canvas.height - this.getHeight()) this.y = canvas.height-this.getHeight();
 	}
 
 }
 
 class Upgrade{
-	constructor(level,cost,name,desc){
-		this.level = level;
+	constructor(cost,name,desc){
+		this.level = 0;
 		/*cost as a sort of multiplier*/
 		this.cost = cost;
 		this.name = name;
@@ -215,7 +275,21 @@ class Upgrade{
 }
 class SpeedUpgrade extends Upgrade{
 	constructor(){
-		super(0,1,'Speed','Make vehicle move faster');
+		super(1,'Speed','Make vehicle move faster');
+
+	}
+	/*return bonus as a decimal multiplier*/
+	getBonus(level=this.level){
+		return level*level/50;
+	}
+	getText(){
+		return `${this.desc} by ${this.getBonus(this.level+1) * 100}%.`;
+	}
+
+}
+class SizeUpgrade extends Upgrade{
+	constructor(){
+		super(1,'Size','Make vehicle bigger');
 
 	}
 	/*return bonus as a decimal multiplier*/
@@ -230,11 +304,42 @@ class SpeedUpgrade extends Upgrade{
 class UpgradeManager{
 	constructor(){
 		this.speedUpgrade = new SpeedUpgrade();
-		this.list = [this.speedUpgrade];
-
+		this.sizeUpgrade = new SizeUpgrade();
+		this.list = [this.speedUpgrade,this.sizeUpgrade];
 	}
 }
 
+class Item{
+	constructor(name,cost){
+		this.name = name;
+		this.cost = cost;
+		this.count = 0;
+	}
+	sell(wallet){
+		let usedCount = (this.count > 5)?5:this.count;
+		wallet.money += this.cost * usedCount;
+		this.count -= usedCount;
+	}
+}
+
+class GrassItem extends Item{
+	constructor(){
+		super('Grass',2);
+		this.img = new Image();
+		this.img.src = 'images/grass.png';
+	}
+
+}
+
+
+class ItemManager{
+	constructor(){
+		this.grass = new GrassItem();
+		this.list = [this.grass];
+		//this.list = [new GrassItem(),new GrassItem(),new GrassItem(),new GrassItem(),new GrassItem(),new GrassItem(),new GrassItem(),new GrassItem(),new GrassItem(),new GrassItem(),new GrassItem(),new GrassItem(),new GrassItem(),new GrassItem(),new GrassItem(),new GrassItem(),new GrassItem(),new GrassItem()];
+	}
+
+}
 
 
 window.addEventListener('load',()=>{
